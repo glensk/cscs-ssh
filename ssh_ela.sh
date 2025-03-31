debd75febed7be489bf6554f881806ce2832e136
glensk <albert.glensk@gmail.com>
2025-03-31 11:56:00 +0200

    2025-03-31__11:56:00__glensk@macwork

--- ssh_ela.sh
+++ ssh_ela.sh
@@ -28,6 +28,9 @@ ssh_ela () {
         echor "my_cscs_username is empty. Please provide it as a first argument. Exit."
         return 1
     fi
+    $cscs_passwords="$2"
+    $cscs_otp="$3"
+
 
 if [ "$USER" != 'glensk' ];then
 if [ "$1" = "" ];then
